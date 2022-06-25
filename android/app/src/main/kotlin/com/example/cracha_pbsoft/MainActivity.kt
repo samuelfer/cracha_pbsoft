7d4f54a8b4e5ea239c64243ebcb04839e321d811
samuelfer <sfsantos@tce.pb.gov.br>
2022-06-25 20:57:10 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/cracha_pbsoft/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cracha_pbsoft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 2.0,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height /
+                      2.0, //Altura do container escuro
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.deepOrange,
+                        width: 3.0,
+                      ),
+                    ),
+                    color: Color.fromARGB(255, 10, 51, 71),
+                  ),
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 100),
+                      child: Column(children: const [
+                        Padding(
+                          padding: EdgeInsets.all(1.0),
+                          child: Text(
+                            'Samuel Fernandes',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(2.0),
+                          child: Text(
+                            'ANALISTA DE SISTEMAS',
+                            style: TextStyle(
+                                color: Colors.deepOrange, fontSize: 13),
+                          ),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //imagens
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 6.5,
+                ),
+                //logo
+                Container(
+                  alignment: Alignment.center,
+                  child: const Image(
+                    image: AssetImage('imagens/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 15.5,
+                ),
+                //foto
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height / 3,
+                  width: MediaQuery.of(context).size.height / 3.5,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.deepOrange,
+                        blurRadius: 3.0,
+                        spreadRadius: 0.1,
+                      ),
+                    ],
+                  ),
+                  // child: const Image(
+                  //   image: AssetImage('imagens/foto.png'),
+                  // ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
